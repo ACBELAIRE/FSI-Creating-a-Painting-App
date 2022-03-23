9297a4773cb236279c7eee444b44b81d4f778984
Your Name <you@example.com>
2022-03-22 21:24:53 -0700

    delete duplicate select the painting

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ painting.addEventListener('click', function(e){
 	console.log('testing, testing, 123')
 })
 
-const painting = document.querySelector('.painting')
-
 painting.addEventListener('click', function(e){
 	console.log(e.target)
 })
