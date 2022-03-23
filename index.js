6e786ea38e69ee893643497a56261458dfb90cf1
Your Name <you@example.com>
2022-03-22 21:21:19 -0700

    reference clicked pixel

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const painting = document.querySelector('.painting')
 painting.addEventListener('click', function(){
 	console.log('testing, testing, 123')
 })
+
+const painting = document.querySelector('.painting')
+
+painting.addEventListener('click', function(e){
+	console.log('testing, testing, 123')
+})
+
+const painting = document.querySelector('.painting')
+
+painting.addEventListener('click', function(e){
+	console.log(e.target)
+})
